77bce43c12094d8126d8b64e7b006399fa3848cf
sathvikbennihalli <74237790+sathvikbennihalli@users.noreply.github.com>
2022-12-26 11:04:05 +0530

    Add files via upload

---
+++ deleteac.php
@@ -0,0 +1,43 @@
+<?php
+include('session.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<div id="contentbox" >
+<script type="text/javascript">
+function countdown() {
+ var i = document.getElementById('timecount');
+ if (parseInt(i.innerHTML)<=1) {
+  location.href = 'index.php';
+ }
+ i.innerHTML = parseInt(i.innerHTML)-1;
+}
+setInterval(function(){ countdown(); },1000);
+</script>
+<?php
+$id=$loggedin_id;
+$temp = $id;
+$sql="DELETE FROM member WHERE mem_id='$id' ";
+$result=mysqli_query($con,$sql);
+if($result){
+
+    $sql = "INSERT INTO deleted_member(mem_id) values( '$temp')";
+    $delete = mysqli_query($con,$sql);
+
+ echo " <div align='center'>";
+ echo "Account Deleted Sucessfully.";
+ echo " <a href='index.php' >Click here</a> to go back. ";
+ echo "</div> ";
+} elseif(!isset($loggedin_session) || $loggedin_session==NULL) {
+ header("Location: index.php");
+} else {
+ echo "Unable to delete Your Account";
+}
+?>
+</div>
+</body>
+</html>
+</div>
---
+++ member.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS `member` (
+  `mem_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `fname` varchar(30) NOT NULL,
+  `lname` varchar(30) NOT NULL,
+  `address` varchar(100) NOT NULL,
+   PRIMARY KEY (`mem_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `deleted_member`(
+`mem_id` int(11) NOT NULL,
+`username` varchar(30) NOT NULL,
+   PRIMARY KEY (`mem_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
